a2212ad235083ff449b726ae69d10a0ea731d0f5
Sean Oughton <seanoughton@gmail.com>
2018-06-28 22:51:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ class Driver {
   };
 
 //returns all of the passengers that a driver has taken on a trip
-//how does a driver know about passengers => through trips 
+//how does a driver know about passengers => through trips
+//all trips by a driver, each trip contains the driverId and the passengerId
   passengers(){
     return store.passengers;//filter this where store.trips
   };
