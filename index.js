2bee95c2346d3921d8bad3225ed8b2828c837e01
Sean Oughton <seanoughton@gmail.com>
2018-06-28 22:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ class Passenger {
     this.name = name;
     store.passengers.push(this);
   };
+
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  };
 };
 
 class Trip {
