c946a8352cd91cf8d901896533f41c8bdf9da20b
Sean Oughton <seanoughton@gmail.com>
2018-06-28 21:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let driverId = 0;
 let passengerId = 0;
+let tripId = 0;
 let store = {drivers:[],passengers:[],trips:[]};
 
 class Driver {
@@ -11,6 +12,14 @@ class Driver {
 };
 
 class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  };
+};
+
+class trip {
   constructor(name) {
     this.id = ++passengerId;
     this.name = name;
